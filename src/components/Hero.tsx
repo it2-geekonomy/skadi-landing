e8e2a6e69909ddcf5be733ff88ea2ed740e1cccb
Sachin Nayak <sachinnayak9108@gmail.com>
2026-07-22 10:48:02 +0530

    stars removed

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,100 +6,14 @@ import Link from "next/link";
 import SectionAurora from "@/components/SectionAurora";
 import HeroReportCard from "@/components/HeroReportCard";
 
-const tinyStars = Array.from({ length: 72 }, (_, i) => ({
-  top: `${((i * 17 + 11) % 94) + 2}%`,
-  left: `${((i * 29 + 7) % 97) + 1}%`,
-  size: i % 4 === 0 ? 2 : 1,
-  opacity: 0.15 + ((i * 13) % 7) * 0.1,
-}));
-
-const twinkleStars = [
-  { top: "18%", left: "76%", size: 14 },
-  { top: "34%", left: "20%", size: 20 },
-  { top: "20%", left: "58%", size: 14 },
-  { top: "20%", left: "7%", size: 14 },
-  { top: "16%", left: "73%", size: 14 },
-  { top: "26%", left: "40%", size: 12 },
-  { top: "51%", left: "66%", size: 12 },
-  { top: "50%", left: "1%", size: 12 },
-];
-
-const mediumStars = [
-  { top: "29%", left: "30%", size: 6, opacity: 1 },
-  { top: "39%", left: "13%", size: 6, opacity: 1 },
-  { top: "16%", left: "15%", size: 6, opacity: 1 },
-  { top: "35%", left: "80%", size: 6, opacity: 0.4 },
-  { top: "47%", left: "20%", size: 6, opacity: 0.4 },
-  { top: "26%", left: "82%", size: 6, opacity: 1 },
-  { top: "29%", left: "71%", size: 6, opacity: 1 },
-  { top: "16%", left: "26%", size: 14, opacity: 0.2 },
-  { top: "75%", left: "11%", size: 14, opacity: 0.54 },
-];
-
 const benefits = [
   "Zero missed opportunities, even after hours",
   "Only qualified leads land on your calendar",
   "Every call scored and tracked, automatically",
 ];
 
-function VectorStar({
-  top,
-  left,
-  size,
-  opacity = 1,
-}: {
-  top: string;
-  left: string;
-  size: number;
-  opacity?: number;
-}) {
-  return (
-    <div
-      className="absolute"
-      style={{ top, left, width: size, height: size, opacity }}
-      aria-hidden="true"
-    >
-      <Image
-        src="/images/Vector.svg"
-        alt=""
-        width={size}
-        height={size}
-        className="h-full w-full"
-      />
-    </div>
-  );
-}
-
 function HeroAurora() {
   return <SectionAurora primaryTop="46%" secondaryTop="52%" />;
-}
-
-function HeroStars() {
-  return (
-    <div className="pointer-events-none absolute inset-0 z-[1]" aria-hidden="true">
-      {tinyStars.map((star, i) => (
-        <div
-          key={`tiny-${i}`}
-          className="absolute rounded-full bg-white"
-          style={{
-            top: star.top,
-            left: star.left,
-            width: star.size,
-            height: star.size,
-            opacity: star.opacity,
-          }}
-        />
-      ))}
-
-      {mediumStars.map((star, i) => (
-        <VectorStar key={`med-${i}`} {...star} />
-      ))}
-
-      {twinkleStars.map((star, i) => (
-        <VectorStar key={`twinkle-${i}`} {...star} />
-      ))}
-    </div>
-  );
 }
 
 function TickIcon() {
@@ -126,7 +40,6 @@ export default function Hero() {
       className="relative overflow-hidden bg-black pb-0 pt-[68px]"
     >
       <HeroAurora />
-      <HeroStars />
 
       <div className="container-main relative z-[2]">
         <div className="flex min-h-[calc(100vh-68px)] w-full flex-col items-center justify-center py-10 lg:py-16">
